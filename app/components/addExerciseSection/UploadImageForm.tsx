cd1a2e261de708567e5893219e0ac3b16b08b449
paHa345 <pav.345@mail.ru>
2023-12-18 10:05:30 +0700

    v 0.40 add add exercise and workout functionality

--- app/components/addExerciseSection/UploadImageForm.tsx
+++ app/components/addExerciseSection/UploadImageForm.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addExerciseActions } from "../../store/addExerciseSlice";
-import IAddExerciseSlice from "../../store/addExerciseSlice";
+import { IAddExerciseSlice } from "../../store/addExerciseSlice";
 const UploadImageForm = ({ setFile, file }: any) => {
   const image = useSelector(
     (state: IAddExerciseSlice) => state.addExerciseState.currentAddedExercise.image
   );
 
+  const dispatch = useDispatch();
+
   const changeUoloadImage = async (e: any) => {
-    setFile((prevState) => {
+    setFile(() => {
       return e.target.files?.[0];
     });
     console.log(file);
