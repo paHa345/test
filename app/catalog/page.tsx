cb995c4a516735cbd73cd5589a4843fd4ed305b3
paHa345 <pav.345@mail.ru>
2024-01-17 11:28:58 +0700

    v 0.535 add dynamic import

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,10 +1,13 @@
 import CatalogMain from "../components/CatalogSection/CatalogMain";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const DynamicCatalogMain = dynamic(() => import("../components/CatalogSection/CatalogMain"));
 
 const catalog = () => {
   return (
     <div className="">
-      <CatalogMain></CatalogMain>
+      <DynamicCatalogMain></DynamicCatalogMain>
     </div>
   );
 };
