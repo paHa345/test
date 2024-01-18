1406358b148c3be9674ac242ed7f455ff2b9550c
paHa345 <pav.345@mail.ru>
2024-01-18 12:17:05 +0700

    v 0.540 test revalidate

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -7,8 +7,24 @@ const catalog = async () => {
   const session = await getServerSession(authOptions);
   console.log(session?.user?.name);
 
+  const posts = await fetch(`${process.env.HOST}/api/exercises/allExercises`, {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const blog = await posts.json();
+
+  const blogEl = blog.result.map((el: any) => {
+    return (
+      <div key={el.name}>
+        <p>{el.name}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="">
+      {blogEl}
       <CatalogMain></CatalogMain>
     </div>
   );
