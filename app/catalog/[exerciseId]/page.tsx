6f5dc1229f510781a38eb44dd531dbf646f1ab71
paHa345 <pav.345@mail.ru>
2024-03-11 18:44:13 +0700

    v 0.853 add on demand revalidation ver 3

--- app/catalog/[exerciseId]/page.tsx
+++ app/catalog/[exerciseId]/page.tsx
@@ -27,7 +27,7 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params: { exerciseId } }: { params: { exerciseId: string } }) {
-  const data = await fetch(`${process.env.HOST}/api/exercises/${exerciseId}`,{  next: { tags: ['collection'] } }
+  const data = await fetch(`${process.env.HOST}/api/exercises/${exerciseId}`,{ cache:"no-cache",  next: { tags: ['collection'] } }
   );
   const exercise = await data.json();
 
