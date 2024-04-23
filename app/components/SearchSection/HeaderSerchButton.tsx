6b16f073b60b97e43dc3f4befa67d2fcba722091
paHa345 <pav.345@mail.ru>
2024-04-23 16:21:14 +0700

    v 0.93511 delete next router8

---
+++ app/components/SearchSection/HeaderSerchButton.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/app/store";
+// import { findExerciseAndSetInState } from "@/app/store/searchExerciseSlice";
+
+const HeaderSerchButton = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const changeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+  const dispatch = useDispatch<AppDispatch>();
+
+  const searchFormSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const params = new URLSearchParams(searchParams);
+    // console.log(params);
+    // if (searchQuery) {
+    //   params.set("query", searchQuery);
+    // } else {
+    //   params.delete("query");
+    // }
+    // if (searchQuery.trim().length < 3) {
+    //   return;
+    // }
+    // if (searchQuery.trim().length !== 0) {
+    //   console.log("replace");
+    //   replace(`/search?query=${searchQuery.trim()}`);
+    //   dispatch(findExerciseAndSetInState(searchParams.get("query")));
+    // }
+  };
+
+  return (
+    <div className=" absolute top-14 right-10 md:right-0  md:static">
+      <form action="submit" onSubmit={searchFormSubmitHandler}>
+        <input
+          className=" px-2 py-2 border border-green-950 rounded-m bg-slate-100 hover:bg-slate-200"
+          type="text"
+          placeholder="Поиск упражнения"
+          onChange={changeSearchQuery}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default HeaderSerchButton;
