3be36c9557f496d7821a6917e90eea15e2ed6e8b
paHa345 <pav.345@mail.ru>
2024-04-23 16:42:46 +0700

    v 0.93514 delete next router11

--- app/components/SearchSection/HeaderSerchButton.tsx
+++ app/components/SearchSection/HeaderSerchButton.tsx
@@ -8,11 +8,11 @@ import { findExerciseAndSetInState } from "@/app/store/searchExerciseSlice";
 const HeaderSerchButton = () => {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState<string | null>("");
 
-  // const changeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearchQuery(e.target.value);
-  // };
+  const changeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
   // const dispatch = useDispatch<AppDispatch>();
 
   const searchFormSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
@@ -41,7 +41,7 @@ const HeaderSerchButton = () => {
           className=" px-2 py-2 border border-green-950 rounded-m bg-slate-100 hover:bg-slate-200"
           type="text"
           placeholder="Поиск упражнения"
-          // onChange={changeSearchQuery}
+          onChange={changeSearchQuery}
         />
       </form>
     </div>
