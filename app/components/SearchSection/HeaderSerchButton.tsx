c1e4fe1144e0d996a61dbc1f0afd1131950d2588
paHa345 <pav.345@mail.ru>
2024-05-28 17:05:44 +0700

    v 0.104

--- app/components/SearchSection/HeaderSerchButton.tsx
+++ app/components/SearchSection/HeaderSerchButton.tsx
@@ -1,54 +1,60 @@
 "use client";
 import React, { useState } from "react";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, usePathname, useRouter, redirect } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/app/store";
-import { findExerciseAndSetInState } from "@/app/store/searchExerciseSlice";
+import { findExerciseAndSetInState, searchExerciseActions } from "@/app/store/searchExerciseSlice";
+// import { searchButtonRedirect } from "@/actions/searchButtonRedirect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const HeaderSerchButton = () => {
-  // const searchParams = useSearchParams();
-  // const { replace } = useRouter();
-  // const [searchQuery, setSearchQuery] = useState<string | null>("");
-
-  // const changeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearchQuery(e.target.value);
-  //   console.log(searchQuery);
-  // };
-
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const { push } = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
   const dispatch = useDispatch<AppDispatch>();
+
+  const changeSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
 
   const searchFormSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // if (searchQuery !== null) {
-    //   const params = new URLSearchParams(searchParams);
-    //   console.log(params);
-    //   if (searchQuery) {
-    //     params.set("query", searchQuery);
-    //   } else {
-    //     params.delete("query");
-    //   }
-    //   if ((searchQuery || "").trim().length < 3) {
-    //     return;
-    //   }
-    //   if ((searchQuery || "").trim().length !== 0) {
-    //     console.log("replace");
-    //     if (searchQuery !== null) {
-    //       replace(`/search?query=${searchQuery.trim()}`);
-    //       dispatch(findExerciseAndSetInState(searchParams.get("query")));
-    //     }
-    //   }
-    // }
+    if (!searchQuery?.trim().length) {
+      return;
+    }
+    if (searchQuery?.trim().length < 3) {
+      return;
+    }
+
+    if (searchQuery !== null) {
+      push(`/search?query=${String(searchQuery).trim()}&page=1`);
+    }
+
+    setSearchQuery("");
   };
 
   return (
     <div className=" absolute top-14 right-10 md:right-0  md:static">
-      <form action="submit" onSubmit={searchFormSubmitHandler}>
+      <form
+        className=" flex rounded-md px-2 py-1 bg-amber-200"
+        action="submit"
+        onSubmit={searchFormSubmitHandler}
+      >
         <input
-          className=" px-2 py-2 border border-green-950 rounded-m bg-slate-100 hover:bg-slate-200"
+          className="rounded-md px-2 py-2 border border-green-950 rounded-m bg-slate-100 hover:bg-slate-200"
           type="text"
           placeholder="Поиск упражнения"
-          // onChange={changeSearchQuery}
+          onChange={changeSearchQuery}
+          value={searchQuery}
         />
+        <button
+          className=" flex items-center ml-2 border-slate-800 border-solid border-2 rounded-md hover:border-double"
+          type="submit"
+        >
+          <FontAwesomeIcon className=" mx-2 my-1 animate-bounce-slow " icon={faMagnifyingGlass} />
+        </button>
       </form>
     </div>
   );
