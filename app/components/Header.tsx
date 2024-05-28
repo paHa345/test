9b5a3aec86c5bca21f72e5cbde53333c4e118025
paHa345 <pav.345@mail.ru>
2024-05-28 15:30:22 +0700

    v 0.100

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,10 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faIdCard, faDumbbell, faMusic } from "@fortawesome/free-solid-svg-icons";
 import { useSession } from "next-auth/react";
 import HeaderSerchButton from "./SearchSection/HeaderSerchButton";
+import { useDispatch } from "react-redux";
+import ReduxProvider from "../ReduxProvider";
 
 const Header = () => {
   const session = useSession();
   return (
+    <ReduxProvider>
     <header className=" bg-mainColor  py-4 pb-16 md:pb-4">
       <nav className=" relative flex flex-row items-center justify-center mx-6 gap-5">
         <Link className=" h-12 mr-12" href="/">
@@ -22,48 +25,48 @@ const Header = () => {
               // layout="responsive"
             ></Image>
           </div>
-        </Link>
-        <HeaderSerchButton></HeaderSerchButton>
-        <div></div>
-        <div className="pr-0 flex justify-end md:pr-10 md:justify-end gap-10 basis-1/2">
-          <div className="">
-            {" "}
-            <Link
-              href="/catalog"
-              className=" text-2xl text-headerButtonColor hover:text-headerButtonHoverColor transition duration-800 ease-out "
-            >
-              {" "}
-              <FontAwesomeIcon icon={faDumbbell} />
-              <p className=" text-xs">Catalog</p>
+            </div>
             </Link>
-          </div>
-          {session.data && (
-            <div>
-              <Link
-                href="/my"
-                className=" text-2xl text-headerButtonColor hover:text-headerButtonHoverColor transition duration-800 ease-out "
-              >
-                <FontAwesomeIcon icon={faIdCard} />
-                <p className=" text-xs">User</p>
-              </Link>
-            </div>
-          )}
-
-          {!session.data && (
-            <div>
+          <HeaderSerchButton></HeaderSerchButton>
+          <div></div>
+          <div className="pr-0 flex justify-end md:pr-10 md:justify-end gap-10 basis-1/2">
+            <div className="">
               {" "}
               <Link
-                href="/login"
+
+                href="/catalog"
                 className=" text-2xl text-headerButtonColor hover:text-headerButtonHoverColor transition duration-800 ease-out "
               >
-                <FontAwesomeIcon className="" icon={faUser} />
-                <p className=" text-xs">Login</p>
               </Link>
             </div>
-          )}
-        </div>
-      </nav>
-    </header>
+            {session.data && (
+              <div>
+                <Link
+                  href="/my"
+                  className=" text-2xl text-headerButtonColor hover:text-headerButtonHoverColor transition duration-800 ease-out "
+                >
+                  <FontAwesomeIcon icon={faIdCard} />
+                  <p className=" text-xs">User</p>
+                </Link>
+              </div>
+            )}
+
+            {!session.data && (
+              <div>
+                {" "}
+                <Link
+                  href="/login"
+                  className=" text-2xl text-headerButtonColor hover:text-headerButtonHoverColor transition duration-800 ease-out "
+                >
+                  <FontAwesomeIcon className="" icon={faUser} />
+                  <p className=" text-xs">Login</p>
+                </Link>
+              </div>
+            )}
+          </div>
+        </nav>
+      </header>
+    </ReduxProvider>
   );
 };
 
